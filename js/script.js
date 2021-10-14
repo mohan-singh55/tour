d6e4ef14cda44eb54abd6d1145f03e5dbc8dea97
mohan-singh55 <chaudharymohan1258@gmail.com>
2021-10-15 04:02:19 +0530

    final project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let navbar = document.querySelector('.navbar');
+let menu = document.querySelector('#menu-bar');
+
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
